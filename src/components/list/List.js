ecc36ecd5b057766806b5f5f006d5ea057936b20
He Yang <heyang0012856@gmail.com>
2019-03-19 16:14:21 +1300

    project init

---
+++ src/components/list/List.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { handleResponse } from '../../helpers';
+import { API_URL } from '../../config';
+import Table from './Table';
+import Loading from '../common/Loading';
+import Pagination from './Pagination';
+
+class List extends Component {
+    constructor(){
+        super();
+        this.state={
+            loading:false,
+            currencies:[],
+            error:null,
+            totalPages:0,
+            page:1,
+        };
+
+        this.handlePaginationClick=this.handlePaginationClick.bind(this);
+    }
+    componentDidMount(){
+      this.fetchCurrencies();
+    }
+    fetchCurrencies(){
+      this.setState({loading:true});
+
+      const {page}=this.state;
+
+      fetch(`${API_URL}/cryptocurrencies?page=${page}&perPage=20`)
+        .then(handleResponse)
+        .then((data) => {
+          console.log(data);
+            const {currencies, totalPages}=data;
+            this.setState({currencies:currencies, totalPages:totalPages, loading:false})
+        })
+        .catch((error) => {
+            this.setState({error:error.errorMessage, loading:false})
+        });
+    }
+    //render percentage
+    renderChangePercent(percent){
+      if(percent>0){
+        return <span className="percent-raised">{percent}% &uarr;</span>
+      } else if(percent<0){
+        return <span className="percent-fallen">{percent}% &darr;</span>
+      }else{
+        return <span>{percent}</span>
+      }
+    }
+
+    handlePaginationClick(direction){
+      let nextPage=this.state.page;
+      //shorter code: nextPage= direction==='next' ? nextPage+1: nextPage-1
+      if(direction==='next'){
+        nextPage++;
+      }else {
+        nextPage--;
+      }
+
+      this.setState({page:nextPage},()=>{
+        //call fetchCurrencies function inside of setState's callback,
+        //because we have to make sure  first page state is updated
+        this.fetchCurrencies();
+      })
+    }
+
+
+    render() {
+      const {loading, error, currencies, totalPages, page}=this.state;
+      //render only  loading component, if loading  state is set to true
+        if(loading){
+           return <div className="loading-container"><Loading /></div>
+        }
+        //render only error message , if error occurred while fetching data
+        if(error){
+          return <div className="error">{error}</div>
+        }
+        return (
+          <div>
+            <Table
+              currencies={currencies}
+              renderChangePercent={this.renderChangePercent}
+            />
+          <Pagination
+            page={page}
+            totalPages={totalPages}
+            handlePaginationClick={this.handlePaginationClick}
+          />
+          </div>
+        );
+    }
+}
+
+export default List;
---
+++ src/helpers.js
@@ -0,0 +1,7 @@
+// fetch error helper
+
+export const handleResponse=(response)=>{
+  return response.json().then(json => {
+      return response.ok ? json : Promise.reject(json);
+  });
+}
